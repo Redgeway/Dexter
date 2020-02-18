2a4f02a98802f4aea2f20c439b128780d0701fb2
Ryan Edgeway <Ryan.Edgeway@IronmongeryDirect.com>
2020-02-18 07:05:12 +0000

    Added basic server functionality

---
+++ Dexter.Server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server by ' + process.env.TEST_VALUE);
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000.');
+})
