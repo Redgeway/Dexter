4a9fe44fd32b9ce8f79c6319f76c11751c290472
Ryan Edgeway <ryan.edgeway@gmail.com>
2020-02-22 11:52:58 +0000

    Root call now returns all firebase data

--- Dexter.Server/server.js
+++ Dexter.Server/server.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-const defaultApp = admin.initializeApp();
+let serviceAccount = require('./assets/serviceAccountKey.json');
+
+const defaultApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 const express = require('express');
 const app = express();
 
 let db = admin.firestore();
 
 app.get('/', function (req, res) {
-    res.send('<b>My</b> first express http server by ' + process.env.FIREBASE_CONFIG);
+    db.collection('pokemon').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log(doc.id, '=>', doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        });    
 });
 
 app.listen(3000, function() {
